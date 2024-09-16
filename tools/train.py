ac498c8b0993a53dba3fbd02b733619a14cc26f0
Martin Biolek <martin@biolek.net>
2024-09-16 12:41:01 +0200

    library versions, docker

--- tools/train.py
+++ tools/train.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # Copyright (C) Alibaba Group Holding Limited. All rights reserved.
+import sys
+
+# Save the command-line arguments to command.txt
+start_command = ' '.join(sys.argv)
+
 import argparse
 import copy
 
@@ -10,6 +15,7 @@ from damo.apis import Trainer
 from damo.config.base import parse_config
 from damo.utils import synchronize
 
+import os
 
 def make_parser():
     """
@@ -44,8 +50,15 @@ def make_parser():
 def main():
     args = make_parser().parse_args()
 
-    torch.cuda.set_device(args.local_rank)
-    torch.distributed.init_process_group(backend='nccl', init_method='env://')
+    local_rank = int(os.getenv('LOCAL_RANK', 0))
+    device = torch.device('cuda', local_rank) if torch.cuda.is_available() else torch.device('cpu')
+    
+    if torch.cuda.is_available():
+        torch.cuda.set_device(local_rank)
+        torch.distributed.init_process_group(backend='nccl', init_method='env://')
+    else:
+        torch.distributed.init_process_group(backend='gloo', init_method='env://')
+    
     synchronize()
     if args.tea_config is not None:
         tea_config = parse_config(args.tea_config)
@@ -55,10 +68,8 @@ def main():
     config = parse_config(args.config_file)
     config.merge(args.opts)
 
-
-    trainer = Trainer(config, args, tea_config)
-    trainer.train(args.local_rank)
-
+    trainer = Trainer(config, args, tea_config,start_command=start_command)
+    trainer.train(local_rank, device)
 
 if __name__ == '__main__':
     main()
